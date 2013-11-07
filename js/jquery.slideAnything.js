d9094124212448abbf09ff2497af47d8dfcfb7eb
Jose Gleiser <jose.gleiser@gmail.com>
2013-11-07 16:47:08 -0300

    Changed slideAnythingFunc to private

--- js/jquery.slideAnything.js
+++ js/jquery.slideAnything.js
@@ -7,53 +7,53 @@
 */
 (function($) {
 
+	var slideAnythingFunc = function ($jsa_wrapper, toggleSpeed, $click) {
+		var
+			$active = $jsa_wrapper.find('.jsa-slider-content.active'),
+			$next = $active.next('.jsa-slider-content'),
+			$clicked,
+			clicked;
+
+		// Change slide by click
+		if ($click) {
+			clicked = $click.attr('href');
+			$clicked = $jsa_wrapper.find(clicked).parent();
+			if (0===$active.find(clicked).length) {
+				$active.toggleClass('active', toggleSpeed);
+				$clicked.toggleClass('active', toggleSpeed);
+				$click.parent().find('.icon-change-slide').removeClass('active');
+				$click.find('.icon-change-slide').addClass('active');
+			}
+		// Go to next slide on timer
+		} else {
+			if (0===$next.length) {
+				$next = $active.siblings(':first');
+			}
+			$active.toggleClass('active', toggleSpeed);
+			$next.toggleClass('active', toggleSpeed);
+			$jsa_wrapper.find('.jsa-controls').find('.icon-change-slide').removeClass('active')
+				.end().find('a[href=#' + $next.children().attr('id') + ']')
+				.find('.icon-change-slide').addClass('active');
+		}
+	};
+
 	$.fn.slideAnything = function( options ) {
 
 		var settings = $.extend( {}, $.fn.slideAnything.defaults, options );
-		
-		var slideAnythingFunc = function ($jsa_wrapper, $click) {
-			var
-				$active = $jsa_wrapper.find('.jsa-slider-content.active'),
-				$next = $active.next('.jsa-slider-content'),
-				$clicked,
-				clicked;
-
-			// Change slide by click
-			if ($click) {
-				clicked = $click.attr('href');
-				$clicked = $jsa_wrapper.find(clicked).parent();
-				if (0===$active.find(clicked).length) {
-					$active.toggleClass('active', settings.toggleSpeed);
-					$clicked.toggleClass('active', settings.toggleSpeed);
-					$click.parent().find('.icon-change-slide').removeClass('active');
-					$click.find('.icon-change-slide').addClass('active');
-				}
-			// Go to next slide on timer
-			} else {
-				if (0===$next.length) {
-					$next = $active.siblings(':first');
-				}
-				$active.toggleClass('active', settings.toggleSpeed);
-				$next.toggleClass('active', settings.toggleSpeed);
-				$jsa_wrapper.find('.jsa-controls').find('.icon-change-slide').removeClass('active')
-				.end().find('a[href=#' + $next.children().attr('id') + ']')
-				.find('.icon-change-slide').addClass('active');
-			}
-		};
 
 		return this.each(function() {
 			var
 				$jsa_wrapper = $(this),
 				interval = settings.timerSpeed,
 				timer = setInterval(function() {
-					slideAnythingFunc($jsa_wrapper);
+					slideAnythingFunc($jsa_wrapper, settings.toggleSpeed);
 				}, interval);
 			$jsa_wrapper.on('click', '.jsa-controls a', function(e) {
 				e.preventDefault();
-				slideAnythingFunc($jsa_wrapper, $(this));
+				slideAnythingFunc($jsa_wrapper, settings.toggleSpeed, $(this));
 				clearInterval(timer);
 				timer = setInterval(function() {
-					slideAnythingFunc($jsa_wrapper);
+					slideAnythingFunc($jsa_wrapper, settings.toggleSpeed);
 				}, interval);
 			});
 		});
